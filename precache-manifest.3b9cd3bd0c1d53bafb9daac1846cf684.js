8650c446b3b80b93d8820d93250d7370a0a18317
hong <gmm117@naver.com>
2020-04-11 17:09:41 +0900

    Updates

---
+++ precache-manifest.3b9cd3bd0c1d53bafb9daac1846cf684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cf9cdca6e64fcc317cfa822a107b86",
+    "url": "/todo-tasks/index.html"
+  },
+  {
+    "revision": "ca7cc84dd35f90f980b5",
+    "url": "/todo-tasks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "047d32d401c61cb71b1a",
+    "url": "/todo-tasks/static/js/2.5578c717.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/todo-tasks/static/js/2.5578c717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7cc84dd35f90f980b5",
+    "url": "/todo-tasks/static/js/main.117baea4.chunk.js"
+  },
+  {
+    "revision": "b5556730921e4cfe2c35",
+    "url": "/todo-tasks/static/js/runtime-main.abc33d76.js"
+  }
+]);
